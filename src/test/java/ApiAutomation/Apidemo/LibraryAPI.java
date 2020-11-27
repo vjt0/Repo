33df1d15ab951247fbf449b02ed27cb695fceefc
vjt0 <46081878+vjt0@users.noreply.github.com>
2020-11-28 01:07:56 +0530

    Update API script

--- src/test/java/ApiAutomation/Apidemo/LibraryAPI.java
+++ src/test/java/ApiAutomation/Apidemo/LibraryAPI.java
@@ -10,6 +10,7 @@ public class LibraryAPI {
 	@Test
 	public void add_book() {
 		RestAssured.baseURI="http://216.10.245.166";
-		given().body(Testap1.input_payload()).when().post("Library/Addbook.php").then().log().all().assertThat().statusCode(200);
+		given().body(Testap1.input_payload()).when().post("Library/Addbook.php").then().log().all()
+		.assertThat().statusCode(200).extract().response().asString();
 	}
 }
